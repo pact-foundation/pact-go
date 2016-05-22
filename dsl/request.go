714946561e8ad56a5a5f65109dd8dc92b7dbe97f
Matt Fellows <matt.fellows@onegeek.com.au>
2016-05-22 18:18:37 +1000

    chore(build): linting and windows deps

--- dsl/request.go
+++ dsl/request.go
@@ -1,3 +1,16 @@
 package dsl
 
-type Request struct{}
+import (
+	"net/http"
+)
+
+// Request contains an API a given Consumer expects from a given Provider.
+type Request interface {
+	Status() int
+	Headers() http.Header
+	Content() interface{}
+}
+
+// PactRequest is the default implementation of the Request interface.
+type PactRequest interface {
+}
--- dsl/response.go
+++ dsl/response.go
@@ -1,3 +1,21 @@
 package dsl
 
-type Response struct{}
+import "net/http"
+
+// Response contains the expectations from an API call from a given Consumer.
+type Response interface {
+	Method() string
+	Path() string
+	Query() string
+	Headers() http.Header
+	Content() interface{}
+}
+
+// PactResponse is the default implementation of the Response interface.
+type PactResponse struct {
+	Method  string
+	Path    string
+	Query   string
+	Headers http.Header
+	content interface{}
+}
