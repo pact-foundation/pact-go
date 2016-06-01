8b52754a606575767ee8c897844d6b8a079e8076
Matt Fellows <matt.fellows@onegeek.com.au>
2016-06-01 22:04:17 +1000

    wip(cli): added flags to pact mock service

--- daemon/pact_mock_service.go
+++ daemon/pact_mock_service.go
@@ -17,10 +17,18 @@ type PactMockService struct {
 // NewService creates a new PactMockService with default settings.
 func (m *PactMockService) NewService() (int, Service) {
 	port, _ := utils.GetFreePort()
+	version := 2
+	dir, _ := os.Getwd()
+	dir = fmt.Sprintf(filepath.Join(dir, "../", "pacts"))
+	logDir := fmt.Sprintf(filepath.Join(dir, "../", "logs"))
 	log.Println("Starting mock service on port:", port)
 
 	m.Args = []string{
 		fmt.Sprintf("--port %d", port),
+		fmt.Sprintf("--pact-specification-version %d", version),
+		fmt.Sprintf("--pact-dir %s", dir),
+		fmt.Sprintf("--log-dir %s", logDir),
+		fmt.Sprintf("--ssl"),
 	}
 	m.Command = getCommandPath()
 	return port, m
