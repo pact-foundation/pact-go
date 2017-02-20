2c6c84ddc321752cc86b35935cf07b154e256265
Matt Fellows <matt.fellows@onegeek.com.au>
2017-02-20 17:53:04 +1100

    fix(windows): update default pact output path

--- dsl/pact.go
+++ dsl/pact.go
@@ -87,7 +87,7 @@ func (p *Pact) Setup() *Pact {
 		args := []string{
 			fmt.Sprintf("--pact-specification-version %d", p.SpecificationVersion),
 			fmt.Sprintf("--pact-dir %s", filepath.FromSlash(p.PactDir)),
-			fmt.Sprintf("--log %s/pact.log", filepath.FromSlash(p.LogDir+"/"+"pact.log")),
+			fmt.Sprintf("--log %s", filepath.FromSlash(p.LogDir+"/"+"pact.log")),
 			fmt.Sprintf("--consumer %s", p.Consumer),
 			fmt.Sprintf("--provider %s", p.Provider),
 		}
