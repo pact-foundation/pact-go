5a6855a0678f0a84da26e29d10583c07395b9f03
Matt Fellows <matt.fellows@onegeek.com.au>
2017-06-12 21:31:58 +1000

    feat(upgrade): upgrade to provider verifier v1.1.2

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,7 +3,7 @@
 set -e
 
 export PACT_MOCK_SERVICE_VERSION=2.1.0 # Seg faults...
-export PACT_PROVIDER_VERIFIER_VERSION=1.1.1
+export PACT_PROVIDER_VERIFIER_VERSION=1.1.3-1
 
 # Create the OS specific versions of the mock service and verifier
 echo "==> Building Ruby Binaries..."
