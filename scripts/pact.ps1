6c7efbd396f59b1b680035a16c11bd1e79e4fa1c
Matt Fellows <matt.fellows@onegeek.com.au>
2017-08-07 19:18:42 +1000

    chore(build): improve error handling for windows builds

--- scripts/pact.ps1
+++ scripts/pact.ps1
@@ -1,5 +1,5 @@
-$pactDir="$env:TEMP\pact"
-
+$pactDir = "$env:TEMP\pact"
+$exitCode = 0
 #rmdir -Recurse -Force $pactDir
 
 Write-Verbose "--> Creating ${pactDir}"
@@ -9,7 +9,7 @@ Write-Verbose "--> Creating pact-go binary"
 go build -o "$pactDir\pact-go.exe" "github.com\pact-foundation\pact-go"
 
 Write-Verbose "--> Creating pact daemon (downloading Ruby binaries)"
-$downloadDir=$env:TEMP
+$downloadDir = $env:TEMP
 $downloadDirMockService = "$pactDir\pact-mock-service"
 $downloadDirPRoviderVerifier = "$pactDir\pact-provider-verifier"
 $url = "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.2.5/pact-1.2.5-win32.zip"
@@ -31,19 +31,17 @@ mv $downloadDirMockService/pact/* $pactDir/pact-mock-service/
 mv $downloadDirPRoviderVerifier/pact/* $pactDir/pact-provider-verifier/
 Get-ChildItem $pactDir
 
-# Time to ensure we fast-fail if a test fails!
-$ErrorActionPreference = "Stop"
-
 Write-Verbose "--> Running tests"
-# $env:PACT_BROKER_HOST="https://test.pact.dius.com.au"
-# $env:PACT_BROKER_USERNAME="dXfltyFMgNOFZAxr8io9wJ37iUpY42M"
-# $env:PACT_BROKER_PASSWORD="O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"
 $packages = go list github.com/pact-foundation/pact-go/... |  where {$_ -inotmatch 'vendor'} | where {$_ -inotmatch 'examples'}
 $curDir=$pwd
 
 foreach ($package in $packages) {
- Write-Verbose "    Running tests for $package"
- go test -v $package
+  Write-Verbose "    Running tests for $package"
+  go test -v $package
+  if ($LastExitCode -ne 0) {
+    Write-Verbose "    ERROR: Test failed, logging failure"
+    $exitCode=1
+  }
 }
 
 Write-Verbose "--> Testing E2E"
@@ -52,6 +50,10 @@ Start-Process -FilePath "$pactDir\pact-go.exe" -ArgumentList "daemon -v -l DEBUG
 $env:PACT_INTEGRATED_TESTS=1
 cd "$env:GOPATH\github.com\pact-foundation\pact-go\dsl"
 go test -v -run TestPact_Integration
+if ($LastExitCode -ne 0) {
+  Write-Verbose "    ERROR: Test failed, logging failure"
+  $exitCode=1
+}
 Write-Verbose "Stop Pact..."
 Stop-Process -Name ruby
 Stop-Process -Name pact-go
@@ -61,17 +63,25 @@ Write-Verbose "    Starting pact daemon in background"
 Start-Process -FilePath "$pactDir\pact-go.exe" -ArgumentList "daemon -v -l DEBUG"  -RedirectStandardOutput "pact.log" -RedirectStandardError "pact-error.log"
 $examples=@("github.com/pact-foundation/pact-go/examples/consumer/goconsumer", "github.com/pact-foundation/pact-go/examples/go-kit/provider", "github.com/pact-foundation/pact-go/examples/mux/provider")
 foreach ($example in $examples) {
-  Write-Verbose "Installing dependencies for example: $example"
+  Write-Verbose "    Installing dependencies for example: $example"
   cd "$env:GOPATH\src\$example"
   go get ./...
-  Write-Verbose "Running tests for $example"
+  Write-Verbose "    Running tests for $example"
   go test -v .
+  if ($LastExitCode -ne 0) {
+    Write-Verbose "    ERROR: Test failed, logging failure"
+    $exitCode=1
+  }
 }
 cd $curDir
 
-Write-Verbose "Shutting down pact processes :)"
+Write-Verbose "    Shutting down pact processes :)"
 Stop-Process -Name ruby
 Stop-Process -Name pact-go
 
 
-Write-Verbose "Done!"
+Write-Verbose "    Done!"
+if ($exitCode -ne 0) {
+  Write-Verbose "--> Build failed, exiting"
+  Exit $exitCode
+}
