7b3fb79e25de41093a9a50bf40242c693b68f1d3
Igor Springer <springerigor@gmail.com>
2018-05-11 09:17:43 +0200

    chore(vendor) Fetch `dep` together with dependencies within build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,10 @@ echo "==> Go version ${VERSION}"
 
 echo "==> Getting dependencies..."
 go get github.com/mitchellh/gox
+# Fetch dependencies using `dep`
+go get github.com/golang/dep/cmd/dep
+dep ensure -v -vendor-only
+
 
 echo "==> Creating binaries..."
 gox -os="darwin" -arch="amd64" -output="build/pact-go_{{.OS}}_{{.Arch}}"
