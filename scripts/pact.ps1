2b35942738985056b97b211620a313ca6180a798
Matt Fellows <matt.fellows@onegeek.com.au>
2020-03-06 21:22:57 +1100

    chore(windows): start docker service

--- scripts/pact.ps1
+++ scripts/pact.ps1
@@ -69,6 +69,7 @@ foreach ($package in $packages) {
 # Run integration tests
 Write-Host "--> Testing E2E examples"
 Write-Host "Running consumer tests"
+docker-compose up -d
 Start-Service docker
 go test -tags=consumer -count=1 github.com/pact-foundation/pact-go/examples/... -run TestExample
 if ($LastExitCode -ne 0) {
