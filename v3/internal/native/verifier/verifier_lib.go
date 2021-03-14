c2a41e812ed556ed9976b25a6a820c139ea91ccf
Matt Fellows <matt.fellows@onegeek.com.au>
2021-03-14 16:26:27 +1100

    chore: allow overriding of the lib install path

--- v3/internal/native/verifier/verifier_lib.go
+++ v3/internal/native/verifier/verifier_lib.go
@@ -1,9 +1,9 @@
 package verifier
 
 /*
-#cgo darwin,amd64 LDFLAGS: -v -lpact_verifier_ffi
-#cgo windows,amd64 LDFLAGS: -v -lpact_verifier_ffi
-#cgo linux,amd64 LDFLAGS: -v -L/usr/local/lib -Wl,-rpath -Wl,/usr/local/lib -lpact_verifier_ffi
+#cgo darwin,amd64 LDFLAGS: -lpact_verifier_ffi
+#cgo windows,amd64 LDFLAGS: -lpact_verifier_ffi
+#cgo linux,amd64 LDFLAGS: -L/tmp -L/usr/local/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,/usr/local/lib -lpact_verifier_ffi
 
 // Mac OSX (until https://github.com/pact-foundation/pact-reference/pull/93 is done)
 // install_name_tool -id "libpact_verifier_ffi.dylib" /usr/local/lib/libpact_verifier_ffi.dylib
