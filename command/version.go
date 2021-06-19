86753f6d9b37a6ad5e386098a84bddbdf0325d7b
Matt Fellows <matt.fellows@onegeek.com.au>
2021-06-19 22:26:03 +1000

    chore: remove temp lib download and fix version

--- command/version.go
+++ command/version.go
@@ -6,14 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "v2.0.0-beta.1"
-var cliToolsVersion = "1.82.3"
+var Version = "v2.0.0-beta.2"
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Pact Go",
 	Long:  `All software has versions. This is Pact Go's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Pact Go CLI %s, using CLI tools version %s", Version, cliToolsVersion)
+		fmt.Printf("Pact Go CLI %s", Version)
 	},
 }
 
