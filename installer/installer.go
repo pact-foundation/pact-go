f2a0b2ad2257dc7013d67dcc45863f2c522a79f6
Matt Fellows <matt.fellows@onegeek.com.au>
2023-05-22 16:07:13 +1000

    fix: upgrade to latest ffi 0.4.4

--- installer/installer.go
+++ installer/installer.go
@@ -377,7 +377,7 @@ const (
 var packages = map[string]packageInfo{
 	FFIPackage: {
 		libName:     "libpact_ffi",
-		version:     "0.4.3",
+		version:     "0.4.4",
 		semverRange: ">= 0.4.0, < 1.0.0",
 	},
 }
