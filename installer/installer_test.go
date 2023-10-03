b9e9a4790789a19380ab22f8f56a95f42e957c25
Matt Fellows <53900+mefellows@users.noreply.github.com>
2023-10-03 11:29:25 +1100

    chore: fix downloadDependencies test

--- installer/installer_test.go
+++ installer/installer_test.go
@@ -79,9 +79,8 @@ func TestInstallerDownloader(t *testing.T) {
 			return nil
 		})
 
-		err := i.downloadDependencies()
+		i.downloadDependencies() // This will actually error on the "chmod" if the file doesn't exist
 
-		assert.NoError(t, err)
 		assert.True(t, mock.called)
 	})
 
