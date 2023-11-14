ddbb56375953107d97aa9ddefad283b1d742dd7b
Stanislav Vodetskyi <49661990+stan-confluent@users.noreply.github.com>
2023-11-14 14:50:50 -0800

    docs: correct link to consumer version selector combinations

--- provider/consumer_version_selector.go
+++ provider/consumer_version_selector.go
@@ -7,7 +7,7 @@ package provider
 // Where a new selector is available in the broker but not yet supported here,
 // you may use the UntypedConsumerVersionSelector to pass in arbitrary key/values
 //
-// Definitive list: https://github.com/pact-foundation/pact_broker/blob/master/spec/lib/pact_broker/api/contracts/verifiable_pacts_json_query_schema_combinations_spec.rb
+// Definitive list: https://github.com/pact-foundation/pact_broker/blob/master/spec/lib/pact_broker/api/contracts/pacts_for_verification_json_query_schema_combinations_spec.rb
 type ConsumerVersionSelector struct {
 	Tag                string `json:"tag,omitempty"`
 	FallbackTag        string `json:"fallbackTag,omitempty"`
