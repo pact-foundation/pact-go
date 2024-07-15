75451f3d7c4353564539b52245a9040ee34e381a
Matt Fellows <53900+mefellows@users.noreply.github.com>
2024-07-15 11:43:54 +1000

    chore(release): release v2.0.6

--- command/version.go
+++ command/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "v2.0.5"
+var Version = "v2.0.6"
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Pact Go",
