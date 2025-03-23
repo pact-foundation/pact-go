a1d79f3ddf78f82cfe0da2efbd30d12a866c5a5d
Matt Fellows <53900+mefellows@users.noreply.github.com>
2025-03-24 09:26:14 +1100

    fix: update pact-ffi to 0.4.27

--- installer/installer.go
+++ installer/installer.go
@@ -388,7 +388,11 @@ const (
 var packages = map[string]packageInfo{
 	FFIPackage: {
 		libName:     "libpact_ffi",
+<<<<<<< HEAD
 		version:     "0.4.26",
+=======
+		version:     "0.4.27",
+>>>>>>> bf039cb (fix: update pact-ffi to 0.4.27)
 		semverRange: ">= 0.4.0, < 1.0.0",
 	},
 }
