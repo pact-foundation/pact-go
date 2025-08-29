375633e076f69c1c5f6174eab43ef3cc989aad4a
YOU54F <YOU54F@users.noreply.github.com>
2025-08-29 10:11:50 +0000

    fix: update pact-ffi to 0.4.28

--- installer/installer.go
+++ installer/installer.go
@@ -388,7 +388,7 @@ const (
 var packages = map[string]packageInfo{
 	FFIPackage: {
 		libName:     "libpact_ffi",
-		version:     "0.4.27",
+		version:     "0.4.28",
 		semverRange: ">= 0.4.0, < 1.0.0",
 	},
 }
