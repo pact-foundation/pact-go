0ffa7c332f0b6c4b4b237bf92ca1d2d8e4159713
Yousaf Nabi <yousaf@pactflow.io>
2026-08-05 15:54:52 +0100

    chore: remove deprecated test

--- internal/native/verifier_test.go
+++ internal/native/verifier_test.go
@@ -18,29 +18,6 @@ func TestVerifier_Version(t *testing.T) {
 	fmt.Println("version: ", Version())
 }
 
-func TestVerifier_Verify(t *testing.T) {
-	t.Run("invalid args returns an error", func(t *testing.T) {
-
-		v := Verifier{}
-		args := []string{
-			"--file",
-			"/non/existent/path.json",
-			"--hostname",
-			"localhost",
-			"--port",
-			"55827",
-			"--state-change-url",
-			"http://localhost:55827/__setup/",
-			"--loglevel",
-			"info",
-		}
-
-		res := v.Verify(args)
-
-		assert.Error(t, res)
-	})
-}
-
 func TestVerifier_NewForApplication(t *testing.T) {
 	v := NewVerifier("pact-go", "test")
 
